71a7656f9dd652b3aea2a12f568c541ad9f26208
Grzegorz_Szwed <greg152295@gmail.com>
2021-05-27 19:43:01 +0200

    added js code to show navbar in button and arrow

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-const conta = document.querySelector('.container');
+const headerButton = document.querySelector('.btn-outline-light');
+const navbar = document.querySelector('.navbar');
+const headerArrow = document.querySelector('.fa-chevron-down');
+
+headerButton.addEventListener('click', ()=>{
+    navbar.classList.add('unhide');
+});
+
+headerArrow.addEventListener('click', ()=>{
+    navbar.classList.add('unhide');
+});
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+    
+//     if (currentScrollPos > 20) {
+//       // I am using 'display' instead of 'top':
+//       document.getElementById("navbar").style.opacity = "1";
+//     } else {
+//       document.getElementById("navbar").style.opacity = "0";
+//     }
+// }
 
