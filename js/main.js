f58935fd6fd53d8b1ceab3c9ba06488ffccb830e
Grzegorz_Szwed <greg152295@gmail.com>
2021-06-04 10:19:39 +0200

    added script to hide navbar after click

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
 let $headerButton;
 let $navbar;
 let $headerArrow;
+let $navItems;
+let $navbarCollapse;
 
 const prepareDOMElemets = () => {
     $headerButton = document.querySelector('.btn-outline-light');
     $navbar = document.querySelector('.navbar');
     $headerArrow = document.querySelector('.fa-chevron-down');
+    $navItems = document.querySelectorAll('.nav-link');
+    $navbarCollapse = document.querySelector('.navbar-collapse');
 }
+
 
 const prepareDOMEvents = () => {
     
     const arrowButtonFunction = () => {
-        navbar.classList.add('unhide');
+        $navbar.classList.add('unhide');
     };
 
     const scroolFunction = () => {
-        if(window.scrollY>=700){
-            navbar.classList.add('unhide');
+        if(window.scrollY>=250){
+            $navbar.classList.add('unhide');
         }else{
-            navbar.classList.remove('unhide');
+            $navbar.classList.remove('unhide');
         }
     };
+
     $headerButton.addEventListener('click',arrowButtonFunction);
     $headerArrow.addEventListener('click',arrowButtonFunction);
     window.addEventListener('scroll',scroolFunction);
+    $navItems.forEach(item => item.addEventListener('click', () => $navbarCollapse.classList.remove('show'))); //hide navbar after click on link
+
 };
 
 const main = () => {
