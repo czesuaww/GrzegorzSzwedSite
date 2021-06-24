e04f088c35495ace295a20af7f797f1f172b53e5
Grzegorz_Szwed <greg152295@gmail.com>
2021-06-24 13:41:56 +0200

    upgraded function when we click on navbar or anywear, the navbar hides

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 let $headerButton;
 let $navbar;
 let $headerArrow;
-let $navItems;
+// let $navItems;
 let $navbarCollapse;
 
 const prepareDOMElemets = () => {
     $headerButton = document.querySelector('.btn-outline-light');
     $navbar = document.querySelector('.navbar');
     $headerArrow = document.querySelector('.fa-chevron-down');
-    $navItems = document.querySelectorAll('.nav-link');
+    // $navItems = document.querySelectorAll('.nav-link');
     $navbarCollapse = document.querySelector('.navbar-collapse');
 }
-
 
 const prepareDOMEvents = () => {
     
@@ -29,10 +28,18 @@ const prepareDOMEvents = () => {
         }
     };
 
+    $(document).click(function (event) {
+        var clickover = $(event.target);
+        var _opened = $(".navbar-collapse").hasClass("show");
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+
     $headerButton.addEventListener('click',arrowButtonFunction);
     $headerArrow.addEventListener('click',arrowButtonFunction);
     window.addEventListener('scroll',scroolFunction);
-    $navItems.forEach(item => item.addEventListener('click', () => $navbarCollapse.classList.remove('show'))); //hide navbar after click on link
+    // $navItems.forEach(item => item.addEventListener('click', () => $navbarCollapse.classList.remove('show'))); //hide navbar after click on link
     window.addEventListener('load', AOS.refresh);
 };
 
